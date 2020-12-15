879aa57c59ddc108f9225ab841b6c498148a5fce
Breno Angelotti <brenogangelotti@gmail.com>
2020-12-15 19:20:24 -0300

    indexing plants to spotlight

--- PlantAR/ViewModels/PlantPickerViewModel.swift
+++ PlantAR/ViewModels/PlantPickerViewModel.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+import CoreSpotlight
+//import MobileCoreServices
+//import UIKit
 
 class PlantPickerViewModel: ObservableObject {
     @Published var plants: [Plant] = []
@@ -36,5 +39,28 @@ class PlantPickerViewModel: ObservableObject {
             Plant(id: "espada_de_sao_jorge", name: "Espada de São Jorge", scientificName: "Dracaena trifasciata", info: "Apodrecerá facilmente se for regado em excesso. É comumente recomendado para iniciantes interessados no cultivo de plantas de interior pela sua facilidade de cuidado.", extraInfo: "Nas Religiões afro-brasileiras, ela é também chamada de espada-de-ogum (quando tem coloração verde) ou espada-de-iansã (bicolor, com bordas amarelas). Esta folha sagrada é uma folha gún (excitante, \"quente\"), sempre presente nos rituais de sasanha e na realização de águas sagradas denominada de abô.", difficulty: 2, water: 1),
             Plant(id: "costela_de_adao", name: "Costela de Adão", scientificName: "Mostera deliciosa", info: "Força e resistência são adjetivos ideais para definir a Costela-de-adão, pois elas podem se adaptar com rapidez a quase todo tipo de ambiente. A espécie tem se tornado uma opção exótica para decorar pequenos jardins, varandas ou mesmo ambientes internos. A Costela de Adão é indicada como a planta do momento pelos profissionais de paisagismo e vem se destacando, também, no meio da moda.", extraInfo: "A espécie é nativa do México e é mundialmente cultivada como ornamental pelas belas e peculiares folhas, com segmentos que lembram costelas. Seu fruto é comestível e muito saboroso, daí seu nome científico, Monstera deliciosa.", difficulty: 1, water: 2)
         ]
+        
+        indexToSpotlight()
+    }
+    
+    func indexToSpotlight(){
+        var items: [CSSearchableItem] = []
+        
+        for plant in plants {
+            let attributes = CSSearchableItemAttributeSet(itemContentType: "com.Angelotti.PlantAR.plant")
+            attributes.title = plant.name
+//            attributes.thumbnailData = UIImage(plant.picture)
+            attributes.contentDescription = plant.extraInfo
+            attributes.identifier = plant.id
+            attributes.relatedUniqueIdentifier = plant.id
+            
+            items.append(CSSearchableItem(uniqueIdentifier: "plant-\(plant.id)", domainIdentifier: "plants", attributeSet: attributes))
+        }
+        
+        CSSearchableIndex.default().indexSearchableItems(items) { (error) in
+            if let error = error {
+                print(error)
+            }
+        }
     }
 }
